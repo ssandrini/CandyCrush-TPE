0b64569861e80400896233ab95782dba3dc29d25
solkonfe <skonfederak@itba.edu.ar>
2020-12-06 20:00:29 -0300

    mejorando levels

--- src/game/backend/level/Level3.java
+++ src/game/backend/level/Level3.java
@@ -1,18 +1,11 @@
 package game.backend.level;
 
 import game.backend.GameState;
-import game.backend.Grid;
-import game.backend.cell.CandyGeneratorCell;
-import game.backend.cell.Cell;
-import game.backend.element.Wall;
-import javafx.scene.paint.Color;
 
-public class Level3 extends Grid {
+public class Level3 extends AbstractLevel {
+    //cambiarlo y poner las condiciones del nivel3(estan con la del 1)
     private static int REQUIRED_SCORE = 5000;
     private static int MAX_MOVES = 20;
-
-    private Cell wallCell;
-    private Cell candyGenCell;
 
     @Override
     protected GameState newState() {
@@ -21,49 +14,14 @@ public class Level3 extends Grid {
 
     @Override
     protected void fillCells() {
-
-        wallCell = new Cell(this);
-        wallCell.setContent(new Wall());
-        candyGenCell = new CandyGeneratorCell(this);
-
-        //corners
-        g()[0][0].setAround(candyGenCell, g()[1][0], wallCell, g()[0][1]);
-        g()[0][SIZE-1].setAround(candyGenCell, g()[1][SIZE-1], g()[0][SIZE-2], wallCell);
-        g()[SIZE-1][0].setAround(g()[SIZE-2][0], wallCell, wallCell, g()[SIZE-1][1]);
-        g()[SIZE-1][SIZE-1].setAround(g()[SIZE-2][SIZE-1], wallCell, g()[SIZE-1][SIZE-2], wallCell);
-
-        //upper line cells
-        for (int j = 1; j < SIZE-1; j++) {
-            g()[0][j].setAround(candyGenCell,g()[1][j],g()[0][j-1],g()[0][j+1]);
-        }
-        //bottom line cells
-        for (int j = 1; j < SIZE-1; j++) {
-            g()[SIZE-1][j].setAround(g()[SIZE-2][j], wallCell, g()[SIZE-1][j-1],g()[SIZE-1][j+1]);
-        }
-        //left line cells
-        for (int i = 1; i < SIZE-1; i++) {
-            g()[i][0].setAround(g()[i-1][0],g()[i+1][0], wallCell ,g()[i][1]);
-        }
-        //right line cells
-        for (int i = 1; i < SIZE-1; i++) {
-            g()[i][SIZE-1].setAround(g()[i-1][SIZE-1],g()[i+1][SIZE-1], g()[i][SIZE-2], wallCell);
-        }
-        //central cells
-        for (int i = 1; i < SIZE-1; i++) {
-            for (int j = 1; j < SIZE-1; j++) {
-                g()[i][j].setAround(g()[i-1][j],g()[i+1][j],g()[i][j-1],g()[i][j+1]);
-                g()[i][j].setColor(Color.SANDYBROWN);
-            }
-        }
+        super.fillCells();
+        // falta rellenar el centro con sandybrown
     }
 
     @Override
     public boolean tryMove(int i1, int j1, int i2, int j2) {
-        boolean ret;
-        if (ret = super.tryMove(i1, j1, i2, j2)) {
-            state().addMove();
-        }
-        return ret;
+        return true;
+        // aca hay que poner si un special move cae en el wall y que lo destruya
     }
 
     private class Level3State extends GameState {
