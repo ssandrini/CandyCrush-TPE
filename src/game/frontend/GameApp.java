e0a1f0e9c9bab8c7bde0e5611b089bf894132bc4
solkonfe <skonfederak@itba.edu.ar>
2020-12-06 12:38:20 -0300

    probando utilizar fxml loader y primer boceto del home del candy crush

--- src/game/frontend/GameApp.java
+++ src/game/frontend/GameApp.java
@@ -4,8 +4,12 @@ import game.backend.CandyGame;
 import game.backend.level.Level1;
 import game.backend.level.Level2;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GameApp extends Application {
 
@@ -14,7 +18,12 @@ public class GameApp extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("PrimerHome.fxml"));
+		Parent root = loader.load();
+
 		CandyGame game = new CandyGame(Level2.class);
 		CandyFrame frame = new CandyFrame(game);
 		Scene scene = new Scene(frame);
